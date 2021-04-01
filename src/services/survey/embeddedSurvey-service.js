c7129c0e9bf1e676c87254722c70ad3a83f11bfe
LucaWeissbeck <lucaweissbeck@yahoo.de>
2021-04-01 09:51:33 +0200

    Added Create Survey Service

--- src/services/survey/embeddedSurvey-service.js
+++ src/services/survey/embeddedSurvey-service.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 
 export const getSurveysByID = (id) => {
     const url = `http://localhost:8080/survey/public/${id}`;
-    return axios.get(url)
-
+    return axios.get(url);
 }
 
 export const getAnswerOptionsByID = (id) => {
