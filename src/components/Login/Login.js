50a1eeba2d85f8ee75b84d076d9cfa1c48ac0aa3
Daniel Ebert <daniel.ebert@ibm.com>
2021-04-02 22:13:22 +0200

    Login is basically functional now.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -52,5 +52,116 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export class Login extends React.Component{
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            email: "",
+            password: "",
+        };
+    }
+
+    setEmail = event => {
+        this.setState({
+            email: event.target.value
+        });
+    };
+
+    setPassword = event => {
+        this.setState({
+            password: event.target.value
+        });
+    };
+
+    componentDidMount() {
+
+    }
+
+    loginActionSubmit = () =>{
+
+        console.log("Test");
+        console.log(this.state.email)
+        console.log(this.state.password)
+        postLogin(this.state.email, this.state.password)
+            .then((res) =>{
+                console.log(res.data.authKey)
+                localStorage.setItem("authKey", res.data.authKey)
+            })
+            .catch(err => console.log(err));
+
+
+    }
+
+    render(){
+        //const { values, handleChangeForm } = props;
+        const styles = theme => ({
+            margin: {
+                margin: theme.spacing.unit * 2,
+            },
+            padding: {
+                padding: theme.spacing.unit
+            }
+        });
+
+        //const ss = useStyles();
+
+        return (
+            <React.Fragment >
+                <Helmet>
+                    <style>{'body { background-color: #D9DCE1; }'}</style>
+                </Helmet>
+                <Container>
+                    <Container style={{marginTop: '15px'}}>
+                        <Card>
+                            <CardHeader
+                                titleTypographyProps={{variant:'h5' }}
+                                avatar={
+                                    <img src="/assets/logo_with_text.png" style={{width: "180px", height:"160px", marginBottom: "-5px"}}/>
+                                }
+                                style={{backgroundColor: "#254563", color: 'white', height: "35px", textAlign: "right"}}>
+                            </CardHeader>
+                            <CardContent>
+                                    <div >
+                                        <Grid container spacing={4} alignItems="flex-end">
+                                            <Grid item>
+                                                <Face />
+                                            </Grid>
+
+                                            <Grid item md={true} sm={true} xs={true}>
+                                                <TextField id="email" label="Email" type="email" fullWidth autoFocus required onChange={this.setEmail} value={this.state.email}/>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid container spacing={4} alignItems="flex-end">
+                                            <Grid item>
+                                                <Fingerprint />
+                                            </Grid>
+                                            <Grid item md={true} sm={true} xs={true}>
+                                                <TextField id="password" label="Password" type="password" fullWidth required onChange={this.setPassword} value={this.state.password}/>
+                                            </Grid>
+                                        </Grid>
+                                        <br/>
+                                        <br/>
+                                        <Grid container alignItems="center" justify="space-between">
+                                            <Grid item>
+                                                <FormControlLabel control={
+                                                    <Checkbox
+                                                        color="primary"
+                                                    />
+                                                } label="Remember me" />
+                                            </Grid>
+                                        </Grid>
+                                        <Grid container justify="center" style={{ marginTop: '10px' }}>
+                                            <Button  color="primary" onClick={this.loginActionSubmit} style={{fontWeight: "bold", textTransform: "none", backgroundColor: "#B4A0B9", color: "white" }}>Login</Button>
+                                            <br/>
+                                        </Grid>
+                                    </div>
+                            </CardContent>
+                        </Card>
+
+                </Container>
+                </Container>
+
+            </React.Fragment>
+        )
+    }
 }
