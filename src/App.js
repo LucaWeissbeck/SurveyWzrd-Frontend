304a1930c46de5802180555a31f97fd3f98efb16
Daniel Ebert <daniel.ebert@ibm.com>
2021-04-02 22:12:59 +0200

    Login is basically functional now.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Embedded} from "./components/Embedded/Embedded";
 import { Analytics } from "./components/Home/Analytics/Analytics";
 import { HowTo } from "./components/Home/HowTo/HowTo";
 import { Overview } from "./components/Home/Overview/Overview";
+import { Login } from "./components/Login/Login";
 import CreateSurvey from "./components/Home/CreateSurvey/CreateSurvey";
 import Home from "./components/Home/Home";
 
@@ -26,6 +27,7 @@ class App extends React.Component {
                         <Switch>
                             <Route path="/" exact component={Home} />
                             <Route path="/survey" component={Embedded} />
+                            <Route path="/login" component={Login} />
                             <Route path="/createsurvey" component={CreateSurvey} />
                             <Route path="/overview" component={Overview} />
                             <Route path="/analytics" component={Analytics} />
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,55 @@
-import react from 'react';
+import React from 'react';
+import * as surveyService from "../../services/overview/overview-service";
+import Header from "../Home/Header/Header";
+import {Helmet} from "react-helmet";
+import {
+    AppBar,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia, Checkbox,
+    Container, FormControlLabel,
+    Grid,
+    IconButton, Paper, Tab, Tabs, TextField,
+    Typography
+} from "@material-ui/core";
+import ShareIcon from "@material-ui/icons/Share";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import {Face, Fingerprint} from "@material-ui/icons";
+import HomeIcon from "@material-ui/icons/Home";
+import {Link} from "react-router-dom";
+import NoteAddIcon from "@material-ui/icons/NoteAdd";
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import HelpIcon from "@material-ui/icons/Help";
+import {makeStyles} from "@material-ui/core/styles";
+import {postLogin} from "../../services/user/login-service";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        border: "none",
+        boxShadow : "none",
+        backgroundColor: '#D9DCE1'
+    },
+    headerOptions: {
+        justifyContent: "space-evenly",
+        display: "flex",
+        flex: 1,
+        fontSize: "1vw",
+        font: "Roboto"
+    },
+    tab:{
+        width: "200px"
+    },
+    navBar:{
+        height: "90px"
+    },
+    menuButton:{
+        marginRight: theme.spacing(2)
+    }
+}));
 
 export class Login extends React.Component{
 
