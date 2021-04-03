a258adfbc48a547088a911895dd2fdc3d7a4fb30
Daniel Ebert <daniel.ebert@ibm.com>
2021-04-03 14:28:06 +0200

    Login is basically functional now.

---
+++ src/services/user/login-service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const postLogin = (email,password) => {
+    let data = JSON.stringify({
+        "email": email,
+        "password": password
+    });
+
+    let config = {
+        method: 'post',
+        url: 'http://localhost:8080/administrator/public/login',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data : data
+    };
+
+    return axios(config);
+
+}
